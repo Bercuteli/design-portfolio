0d58bde2adc5b30771bc5dbf25dcbd5fa796f53c
Bercuteli <bercuteli@gmail.com>
2020-10-23 16:35:07 +0300

    Added redirect for deep routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,15 +1,25 @@
 import React, { FC, Suspense, lazy } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import { config } from '../config';
 import { RoutesUI } from '../enums/routes';
 import { ProjectsList } from '../containers/ProjectsList';
 import { Wrapper } from './Routes.style';
-import { config } from '../config';
 
 const About = lazy(() => import('../pages/About'));
 const Project = lazy(() => import('../pages/Project'));
 
 const Routes: FC = () => {
+
+  // Due to GitHub Pages serves only static files, it can't response with index.html for every request
+  // so, we have to use this workaround.
+  // See more details in /public/404.html
+  const search = new URLSearchParams(window.location.search);
+  const redirectURL = search.get('p');
+  if (redirectURL) {
+    const targetURL = `${window.location.origin}${config.publicURL}${redirectURL}`;
+    window.location.replace(targetURL);
+  }
 
   return (
     <BrowserRouter basename={config.publicURL}>
