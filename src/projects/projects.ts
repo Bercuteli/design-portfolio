1432892e1181b04c97cbe8b1bfce8f09dcfa3545
Bercuteli <bercuteli@gmail.com>
2020-11-04 21:48:45 +0200

    Added Cats-System design

--- src/projects/projects.ts
+++ src/projects/projects.ts
@@ -3,19 +3,14 @@ import { Projects } from '../enums/projects';
 import { ImageSizes } from '../enums/imageSizes';
 
 // cats-system
-import CatsSystemPreviewMini from './catsSystem/preview-mini.jpg';
-import CatsSystemSketchOrigin from './catsSystem/sketch-origin.jpg';
-import CatsSystemSketchMiddle from './catsSystem/sketch-middle.jpg';
-import CatsSystemSketchSmall from './catsSystem/sketch-small.jpg';
-import CatsSystemDesignOrigin from './catsSystem/design-origin.jpg';
-import CatsSystemDesignMiddle from './catsSystem/design-middle.jpg';
-import CatsSystemDesignSmall from './catsSystem/design-small.jpg';
+import CatsSystemPreviewMini from './catsSystem/preview-mini.png';
+import CatsSystemSketchOrigin from './catsSystem/sketch-origin.png';
+import CatsSystemSketchMiddle from './catsSystem/sketch-middle.png';
+import CatsSystemSketchSmall from './catsSystem/sketch-small.png';
+import CatsSystemDesignOrigin from './catsSystem/design-origin.png';
+import CatsSystemDesignMiddle from './catsSystem/design-middle.png';
+import CatsSystemDesignSmall from './catsSystem/design-small.png';
 import CatsSystemHTML from './catsSystem/html';
-
-// dog-store
-import EmptyTemplate from '../images/empty-template-1440-900.png';
-import DogStoreDesign from './dogStore/design.jpg';
-import DogStoreHTML from './dogStore/html';
 
 export const projects: Array<Project> = [
   {
@@ -34,21 +29,5 @@ export const projects: Array<Project> = [
       [ImageSizes.small]: CatsSystemDesignSmall,
     },
     html: CatsSystemHTML,
-  },
-  {
-    id: Projects.dogStore,
-    name: 'Dog Store',
-    description: 'Everything for your dogs',
-    sketch: {
-      [ImageSizes.origin]: EmptyTemplate,
-      [ImageSizes.middle]: EmptyTemplate,
-      [ImageSizes.small]: EmptyTemplate,
-    },
-    design: {
-      [ImageSizes.origin]: DogStoreDesign,
-      [ImageSizes.middle]: DogStoreDesign,
-      [ImageSizes.small]: DogStoreDesign,
-    },
-    html: DogStoreHTML,
   },
 ];
